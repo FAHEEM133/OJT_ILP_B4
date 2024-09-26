eb8da329bc0839a88b03ab288a0e7c2fcaf3fea0
pgsaihari <94387222+pgsaihari@users.noreply.github.com>
2024-09-26 15:49:18 +0530

    Revert "Bil 11 create market api"

--- Application/Requests/RegionRequests/GetAllRegionsQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using System.Collections.Generic;
-
-namespace Application.Requests.RegionRequests
-{
-    public class GetAllRegionsQuery : IRequest<List<KeyValuePair<int, string>>>
-    {
-    }
-}
