915bbcc213c504c8b0f4e186554ba00181a4b738
FAHEEM133 <mhd.faheem00@gmail.com>
2024-10-17 15:01:09 +0530

    DTO optimized

--- Application/DTOs/MarketSubGroupDTO.cs
+++ Application/DTOs/MarketSubGroupDTO.cs
@@ -1,9 +1,15 @@
-﻿namespace Application.DTOs;
+﻿using System.Text.Json.Serialization;
+
+namespace Application.DTOs;
 
 public class MarketSubGroupDTO
 {
+    [JsonIgnore]
     public int SubGroupId { get; set; }
     public string SubGroupName { get; set; }
     public string SubGroupCode { get; set; }
+
+    [JsonIgnore]
     public int MarketId { get; set; }
+
 }
