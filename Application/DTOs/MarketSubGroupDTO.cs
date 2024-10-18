670742e68f1fbb92bcd3bd522a84697e5507de75
pgsaihari <saiharinarayan@gmail.com>
2024-10-18 15:32:54 +0530

    "isDeleted and isUpdated change added in the update logic"

--- Application/DTOs/MarketSubGroupDTO.cs
+++ Application/DTOs/MarketSubGroupDTO.cs
@@ -8,11 +8,14 @@ namespace Application.DTOs
 {
     public class MarketSubGroupDTO
     {
+        
         public int SubGroupId { get; set; }
         public string SubGroupName { get; set; }
         public string SubGroupCode { get; set; }
         public int MarketId { get; set; }
 
-        public bool IsDeleted { get; set; }=false;
+        public bool IsDeleted { get; set; } = false;
+
+        public bool IsEdited { get; set; } = false;
     }
 }
