702ce302aa39622478a80ecdc0b67c59927ce635
irshadhussain21 <irshadhussain120@gmail.com>
2024-10-21 17:57:16 +0530

    config update

--- Infrastructure/ConfigurationServices.cs
+++ Infrastructure/ConfigurationServices.cs
@@ -11,7 +11,7 @@ namespace Infrastructure
         {
             
             services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
 
 
